c858d01e19678c3410131ad7ac610c25d3b23281
deckard <deckard@deckard.(none)>
2017-08-13 14:29:18 +0200

    added get_server_time and started get_asset_info

---
+++ R/get_asset_info.R
@@ -0,0 +1,9 @@
+#'@title
+#'@description
+# https://api.kraken.com/0/public/Assets?info='currency'
+
+get_asset_info <- function() {
+
+
+
+}
---
+++ R/get_server_time.R
@@ -0,0 +1,22 @@
+#'@title gets the server time.
+#'@description aid to aaproximating the skew time between server and client.
+#'returns unix timestamp and time in RFC1123 format
+get_server_time <- function() {
+
+  serv_time_results <- jsonlite::fromJSON("https://api.kraken.com/0/public/Time")
+
+  # if an error message is returned handle it
+  # and return all the data
+  err <- serv_time_results$error
+  if(length(err)>0) {
+    cat("Error received retrieving data")
+    return(serv_time_results)
+
+  # otherwise return the two time formats
+  } else {
+
+    serv_time <- serv_time_results$result
+  }
+
+  return(serv_time)
+}
