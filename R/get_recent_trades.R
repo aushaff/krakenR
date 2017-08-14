cb0db325ff49c15a2f9d9cefaa2e901b192582e5
deckard <deckard@deckard.(none)>
2017-08-14 14:03:30 +0200

    get_recent_trades updated

--- R/get_recent_trades.R
+++ R/get_recent_trades.R
@@ -1,11 +1,30 @@
-#  https://api.kraken.com/0/public/Trades
+#'@title gets recent trades from a 'since' date
+#'@description raw data return in form of:
+#'pair name plus:
+#' <price>, <volume>, <time>, <buy/sell>, <market/limit>, <miscellaneous>
+#' and 'last'. the id to be used as since when polling for new trade data
+#'@param base_url https://api.kraken.com/0/public/Trades
 #
-get_recent_trades <- function(pair = "XBTEUR") {
+get_recent_trades <- function(pair = "XBTEUR",
+                              since = 0) {
 
   base_url <- "https://api.kraken.com/0/public/Trades"
   url <- paste0(base_url, "?", "pair=", pair)
 
-  trades_out <- jsonlite::fromJSON(url)
+  trades_ret<- jsonlite::fromJSON(url)
+
+  # if an error message is returned handle it
+  # and return all the data
+  err <- trades_ret$error
+  if(length(err)>0) {
+    cat("Error received retrieving data")
+    return(trades_ret)
+
+    # otherwise return the data and the last time
+  } else {
+
+    trades_out <- trades_ret$result
+  }
 
   return(trades_out)
 }
