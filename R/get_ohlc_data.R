9a347effeda95de1a772a8ced9e621d753e2207c
deckard <haffamoto@yahoo.co.uk>
2017-12-05 21:04:14 +0100

    added scale for ohlc

--- R/get_ohlc_data.R
+++ R/get_ohlc_data.R
@@ -1,3 +1,7 @@
+# time frame interval in minutes:
+# 1 (default), 5, 15, 30, 60, 240 (4 hour),
+# 1440 (24 hour), 10080 (1 week), 21600 (15 days?)
+
 #  https://api.kraken.com/0/public/OHLC
 # can be a service unavailabe error if too soon
 get_ohlc_data <- function(pair = "XBTEUR",
